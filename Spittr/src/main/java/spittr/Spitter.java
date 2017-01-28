b757938cd5c2b7b40e9d48eb0897d2676efe41e6
rajan <rajan@DESKTOP-OUS5T61>
2017-01-28 16:48:35 +0530

    Form validation @NotNull & @Size, use of Hibernate Validator a java JSR303 specification

--- Spittr/src/main/java/spittr/Spitter.java
+++ Spittr/src/main/java/spittr/Spitter.java
@@ -1,10 +1,21 @@
 package spittr;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Spitter {
 	
+	@NotNull
+	@Size(min=5,max=10)
 	private String firstName;
+	@NotNull
+	@Size(min=5,max=10)
 	private String lastName;
+	@NotNull
+	@Size(min=5,max=10)
 	private String userName;
+	@NotNull
+	@Size(min=5,max=10)
 	private String password;
 	
 	public String getFirstName() {
